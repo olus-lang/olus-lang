fcf005cca60d20eaf85c28c7f2d49b8cf9b95e4f
Remco Bloemen <remco@wicked.ventures>
2018-07-16 17:14:01 +0200

    Look forward in blocks when binding

--- olus-lang/src/Binder.hs
+++ olus-lang/src/Binder.hs
@@ -3,11 +3,12 @@ module Binder where
 
 import Prelude hiding (lookup)
 import Control.Monad.State
-import Data.Map.Strict hiding (map, foldr)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import qualified Syntax as S
 
 empty :: Map String Int
-empty = Data.Map.Strict.empty
+empty = Map.empty
 
 maxBinderNumber :: S.Scope -> Int
 maxBinderNumber = fScope where
@@ -58,17 +59,46 @@ numberBinders start scope = evalState (numberScope scope) start where
     put (n + 1)
     return $ S.Binder s n
 
--- TODO: Forward looking
+forwardReferences :: [S.Scope] -> Map String Int
+forwardReferences scope = execState (forwardScope scope) Map.empty where
+  forwardScope :: [S.Scope] -> State (Map String Int) ()
+  forwardScope = mapM_ $ \case
+    S.Block b -> return () -- No recursion into blocks
+    S.Declaration a b c -> do
+      forwardBinder a
+      mapM_ forwardBinder b
+      mapM_ forwardExpression c
+    S.Statement a -> do
+      mapM_ forwardExpression a
+  forwardExpression :: S.Expression -> State (Map String Int) ()
+  forwardExpression = \case
+    S.Fructose a b -> do
+      mapM_ forwardBinder a
+      mapM_ forwardExpression b
+    S.Galactose a -> do
+      mapM_ forwardExpression a
+    _ -> return ()
+  forwardBinder :: S.Binder -> State (Map String Int) ()
+  forwardBinder (S.Binder s n) = do
+    m <- get
+    put $ Map.insertWith (\old new -> old) s n m
+
 bindReferences :: Map String Int -> S.Scope -> S.Scope
 bindReferences initial scope = evalState (bindScope scope) initial where
   bindScope :: S.Scope -> State (Map String Int) S.Scope
   bindScope = \case
     S.Block b -> do
+      -- Store current environment on stack
       env <- get
+      -- Add forward references, preferring over outer references
+      put $ Map.union (forwardReferences b) env
+      -- Handle body of block
       b' <- mapM bindScope b
+      -- Recover current environment from before block
       put env
       return $ S.Block b'
     S.Declaration a b c -> do
+      -- TODO: If the next item is a block, include its forward declarations in the environment for this declaration. For this it's probably best to modify the AST to have an S.DeclarationWithBlock.
       a' <- bindBinder a
       b' <- mapM bindBinder b
       c' <- mapM bindExpression c
@@ -87,14 +117,14 @@ bindReferences initial scope = evalState (bindScope scope) initial where
       return $ S.Galactose a'
     S.Reference s n -> do
       env <- get
-      case lookup s env of
+      case Map.lookup s env of
         Just n' -> return $ S.Reference s n'
         Nothing -> return $ S.Reference s n
     a -> return a
   bindBinder :: S.Binder -> State (Map String Int) S.Binder
   bindBinder (S.Binder s n) = do
     env <- get
-    put $ insert s n env
+    put $ Map.insert s n env
     return $ S.Binder s n
 
 flattenScopes :: S.Scope -> S.Scope
@@ -106,5 +136,5 @@ flattenScopes s = S.Block $ f s where
 bindingPass :: Int -> S.Scope -> S.Scope
 bindingPass offset scope =
   flattenScopes $
-  bindReferences (fromList []) $
+  bindReferences (Map.fromList []) $
   numberBinders offset scope 

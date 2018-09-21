6d48a8663a323b0c3804c8c35efb4082a7087ae5
Remco Bloemen <remco@wicked.ventures>
2018-09-21 15:38:09 +0200

    Cleanup constant extraction

--- src/Olus/Ast/Conversions/ToIr.hs
+++ src/Olus/Ast/Conversions/ToIr.hs
@@ -4,49 +4,71 @@ import Data.Maybe (fromJust)
 import Data.List (elemIndex, nub)
 
 import qualified Olus.Ast.Syntax as S
-import qualified Olus.Ir.Program as P
 import qualified Olus.Ast.Binder as B
-import qualified Olus.Ast.Passes.Desugar as D
 import qualified Olus.Ast.Passes.ConstantExtraction as CE
+import qualified Olus.Ast.Passes.Desugar as D
+import qualified Olus.Ir.Program as P
+import qualified Olus.Ir.Intrinsics as I
+
+convertConstant :: (String -> Maybe Int) -> S.Expression -> P.Constant
+convertConstant builtinLookup = \case
+  S.LiteralInteger n  -> P.Integer n
+  S.LiteralString s   -> P.String s
+  S.Reference s _     -> case builtinLookup s of
+    Just i   -> P.Intrinsic i
+    Nothing  -> error "Builtin not found"
+  _                   -> error "Expression is not a constant"
 
 extractDeclarations :: S.Scope -> [([Int],[Int])]
 extractDeclarations = fScope where
   fScope :: S.Scope -> [([Int],[Int])]
   fScope = \case
-    S.Block a -> concatMap fScope a
-    S.Declaration a b c -> [(map fBinder $ a:b, map fExpression c)]
-    _ -> []
+    S.Block a            -> concatMap fScope a
+    S.Declaration a b c  -> [(map fBinder $ a:b, map fExpression c)]
+    _                    -> []
+  
   fExpression :: S.Expression -> Int
   fExpression = \case
-    S.Reference _ n -> n
-    _ -> error "Expecting only References in declarations"
+    S.Reference _ n  -> n
+    _                -> error "Expecting only References in declarations"
+  
   fBinder :: S.Binder -> Int
   fBinder (S.Binder _ n) = n
 
 extractStatements :: S.Scope -> [[Int]]
 extractStatements = fScope where
+  
   fScope :: S.Scope -> [[Int]]
   fScope = \case
-    S.Block a -> concatMap fScope a
-    S.Statement a -> [map fExpression a]
-    _ -> []
+    S.Block a      -> concatMap fScope a
+    S.Statement a  -> [map fExpression a]
+    _              -> []
+  
   fExpression :: S.Expression -> Int
   fExpression = \case
-    S.Reference _ n -> n
-    _ -> error "Expecting only References in declarations"
+    S.Reference _ n  -> n
+    _                -> error "Expecting only References in declarations"
 
 extractIdentifiers :: S.Scope -> [String]
 extractIdentifiers = fScope where
+  
   fScope :: S.Scope -> [String]
   fScope = \case
-    S.Block a -> concatMap fScope a
-    S.Declaration a b c -> map fBinder $ a:b
-    _ -> []
+    S.Block a            -> concatMap fScope a
+    S.Declaration a b c  -> map fBinder $ a:b
+    _                    -> []
+  
   fBinder :: S.Binder -> String
   fBinder (S.Binder s _) = s
 
 toIr :: S.Scope -> P.Program
 toIr scope = program where
+  
+  isBuiltin :: String -> Bool
+  isBuiltin = undefined
+  
+  builtinLookup :: String -> Maybe Int
+  builtinLookup = flip elemIndex I.intrinsics
   
   -- Bind references counting creating indices from zero.
   -- Flatten scopes to a single block.
@@ -57,7 +79,9 @@ toIr scope = program where
   
   -- Turn constants into references. Indices start from zero.
   -- Existing indices are shifted up.
-  (constants, scope''') = CE.extractConstants scope''
+  (constants, scope''') = CE.extractConstants isBuiltin scope''
+  
+  constants' = map (convertConstant builtinLookup) constants
   
   -- Extract declarations and statements
   declarations = extractDeclarations scope'''
@@ -65,7 +89,7 @@ toIr scope = program where
   identifiers = (map show constants) ++ extractIdentifiers scope'''
   
   program = P.empty {
-    P.constants = constants,
+    P.constants = constants',
     P.declarations = declarations,
     P.statements = statements,
     P.identifiers = identifiers
--- src/Olus/Ast/Passes/ConstantExtraction.hs
+++ src/Olus/Ast/Passes/ConstantExtraction.hs
@@ -4,46 +4,46 @@ import Data.List (elemIndex, nub)
 import Data.Maybe (fromJust)
 
 import Olus.Ast.Syntax as S
-import Olus.Ir.Program as P
-import Olus.Ir.Intrinsics as I
 
 -- NOTE: It will bind any unboud references that match intrinsics
-listConstants :: S.Scope -> [P.Constant]
-listConstants = nub . listBlock where
-  listBlock :: S.Scope -> [P.Constant]
+listConstants :: (String -> Bool) -> S.Scope -> [S.Expression]
+listConstants isBuiltin = nub . listBlock where
+  
+  listBlock :: S.Scope -> [S.Expression]
   listBlock = \case
-    S.Block a           -> concatMap listBlock a
-    S.Declaration _ _ a -> concatMap listExp a
-    S.Statement a       -> concatMap listExp a
-  listExp :: S.Expression -> [P.Constant]
-  listExp = \case
-    S.LiteralInteger n -> [P.Integer n]
-    S.LiteralString s -> [P.String s]
-    S.Reference s (-1) -> case I.intrinsicIndex s of
-      Just i -> [P.Intrinsic i]
-      Nothing -> []
-    _ -> []
+    S.Block a            -> concatMap listBlock a
+    S.Declaration _ _ a  -> concatMap listExp a
+    S.Statement a        -> concatMap listExp a
+  
+  listExp :: S.Expression -> [S.Expression]
+  listExp ex = case ex of
+    S.LiteralInteger _  -> [ex]
+    S.LiteralString _   -> [ex]
+    S.Reference s (-1)  -> if isBuiltin s then [ex] else []
+    _                   -> []
 
-referenceConstants :: (P.Constant -> Int) -> S.Scope -> S.Scope
+referenceConstants :: (S.Expression -> Maybe Int) -> S.Scope -> S.Scope
 referenceConstants refmap = mapBlock where
+  
   mapBlock :: S.Scope -> S.Scope
   mapBlock = \case
-    S.Block a           -> S.Block $ map mapBlock a
-    S.Declaration a b c -> S.Declaration a b $ map mapExp c
-    S.Statement a       -> S.Statement $ map mapExp a
+    S.Block a            -> S.Block $ map mapBlock a
+    S.Declaration a b c  -> S.Declaration a b $ map mapExp c
+    S.Statement a        -> S.Statement $ map mapExp a
+  
   mapExp :: S.Expression -> S.Expression
-  mapExp = \case
-    S.LiteralInteger n -> S.Reference (show n) $ refmap $ P.Integer n
-    S.LiteralString s -> S.Reference (show s) $ refmap $ P.String s
-    S.Reference s (-1) -> case I.intrinsicIndex s of
-      Just i -> S.Reference s $ refmap $ P.Intrinsic i
-      Nothing -> S.Reference s (-1)
-    a -> a
+  mapExp ex = case ex of
+    S.LiteralInteger n  -> S.Reference (show n) $ fromJust $ refmap ex
+    S.LiteralString s   -> S.Reference (show s) $ fromJust $ refmap ex
+    S.Reference s (-1)  -> case refmap ex of
+      Just i   -> S.Reference s i
+      Nothing  -> S.Reference s (-1)
+    a                   -> a
 
-extractConstants :: S.Scope -> ([P.Constant], S.Scope)
-extractConstants s = let
-    consts = listConstants s
+extractConstants :: (String -> Bool) -> S.Scope -> ([S.Expression], S.Scope)
+extractConstants isBuiltin s = let
+    consts = listConstants isBuiltin s
     s' = S.renumber' (length consts +) s -- Shift indices up
-    refmap con = fromJust (elemIndex con consts)
+    refmap con = elemIndex con consts
     s'' = referenceConstants refmap s'
   in (consts, s'')
